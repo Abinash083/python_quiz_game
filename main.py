2f1e7b45aeae9f09d28eb20aee5f6e8829632839
Abinash <avinashniure026@gmail.com>
2022-02-17 18:39:38 +0930

    starting class

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from data import question_data
+from question_model import Question
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+question_bank = []
+
+for item in question_data:
+    new_question = Question(item["text"], item["answer"])
+    question_bank.append(new_question)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
